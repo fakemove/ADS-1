555987878aac110154009edf8ceda8186e4757d0
fakemove <160417424+fakemove@users.noreply.github.com>
2024-03-01 11:10:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,49 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	for (int i = 2; i < value; i++) {
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t k = 0;
+	uint64_t num = 2;
+	while (k != n) {
+		if (checkPrime(num)) {
+			k += 1;
+		}
+		if (k == n) {
+			return num;
+		}
+		num += 1;
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t k = 0;
+	for (uint64_t i = value + 1; ; i++) {
+		if (k == 1) {
+			return i-1;
+		}
+		k = 0;
+		for (uint64_t j = 2; j <= i; j++) {
+			if (i % j == 0) {
+				k += 1;
+			}
+		}	
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++) {
+	  if (checkPrime(i)) {
+	  sum += i;
+	  }
+  }
+  return sum;
 }
