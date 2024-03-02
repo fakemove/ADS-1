88c2265bc5e232ee2bc81e92122212957794f820
fakemove <160417424+fakemove@users.noreply.github.com>
2024-03-02 20:40:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,49 +4,38 @@
 
 
 bool checkPrime(uint64_t value) {
-	for (int i = 2; i < value; i++) {
-		if (value % i == 0) {
-			return false;
-		}
-	}
-	return true;
+// вставьте код функции
+for (uint64_t i = 2; i < value; i++)
+  if (value % i == 0)
+    return false;
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t k = 0;
-	uint64_t num = 2;
-	while (k != n) {
-		if (checkPrime(num)) {
-			k += 1;
-		}
-		if (k == n) {
-			return num;
-		}
-		num += 1;
-	}
+// вставьте код функции
+uint64_t* array = new uint64_t[n]{};
+uint64_t counter = 0;
+for (uint64_t i = 2; array[n - 1] == 0; i++)
+  if (checkPrime(i))
+    array[counter++] = i;
+uint64_t res = array[n - 1];
+delete[] array;
+return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t k = 0;
-	for (uint64_t i = value + 1; ; i++) {
-		if (k == 1) {
-			return i-1;
-		}
-		k = 0;
-		for (uint64_t j = 2; j <= i; j++) {
-			if (i % j == 0) {
-				k += 1;
-			}
-		}	
-	}
+// вставьте код функции
+for (uint64_t i = value + 1; ; i++)
+  if (checkPrime(i))
+    return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (int i = 2; i < hbound; i++) {
-	  if (checkPrime(i)) {
-	  sum += i;
-	  }
+// вставьте код функции
+  uint64_t res = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i))
+      res += i;
   }
-  return sum;
+    return res;
 }
